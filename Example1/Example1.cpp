cf63f8cd687b1dbc4fc069f62470edfc0f4a9c59
a233ahme <aamna.ahmed@ryerson.ca>
2015-10-23 17:01:07 -0400

    added the source file and code

---
+++ Example1/Example1.cpp
@@ -0,0 +1,34 @@
+/*
+ * Example1.cpp
+ *
+ *  Created on: Oct 23, 2015
+ *      Author: a233ahme
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+int patient[20];
+int *ptr=patient;
+int i;
+
+for ( i=0;i<5;++i)
+{
+	cout <<"Enter Patient: ";
+	cin >> ptr[i];
+
+
+}
+for (i=0;i<5;++i)
+{
+	++*ptr;
+}
+
+//I made changes
+
+return 0;
+}
+
+
+
